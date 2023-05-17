32ae3c025e99a14560617f3b1b975ca1b5f43806
Mofam-01 <finemypath@gmail.com>
2023-05-17 11:40:16 -0700

    builtin.c

--- builtin.c
+++ builtin.c
@@ -29,3 +29,65 @@ int_myexit(info_t *info)
 	info->err_num = -1;
 	return (-2);
 }
+
+/**
+ * _mycd - the process of the current directory changes
+ * @info: Structure that contain potential arguments. Used to maintain
+ * a constant function prototype
+ * Return: Always 0
+ */
+
+int_mycd(info_t *info)
+{
+	char *s, *dir, buffer[1024];
+	int chdir_ret;
+
+	s = getcwd(buffer, 1024);
+	if (!s)
+		_puts("TODO:>>getcwd failure emsg here<<\n");
+	if (!info->argv[1])
+	{
+		dir = _getenv(info, "HOME=")
+		if (!dir)
+			chdir_ret = /* TODO: what should this be? */
+				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
+		else
+			chdir_ret = chdir(dir);
+	}
+	else if (_strcmp(info->argv[1], "-") == 0)
+	{
+		if (!_getenv(info, "OLDPWD="))
+		{
+			_puts(s);
+			_putchar('\n');
+			return (1);
+		}
+		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
+		chdir_ret = /* TODO: what should this be? */
+			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
+	}
+	else
+	{
+		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
+		_setenv(info, "PWD", getcwd(buffer, 1024));
+	}
+	return (0);
+}
+
+/**
+ * _myhelp - current directory of the process changes
+ * @info: Structure contain potential arguments. Used to maintain
+ * a constant function prototype.
+ * Return: Always 0
+ */
+
+int_myhelp(info_t *info)
+{
+	char **arg_array;
+
+	arg_array = info->argv;
+	_puts("help call works. Function not yet implemented \n");
+	if (0)
+		_puts(*arg_array); /* temp att_unused workaround */
+	return (0);
+}
